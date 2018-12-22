a856d7ade02492273fef696b8914bfb5429ed34f
Glitch (pumped-orangutan) <none>
2018-12-22 16:26:01 +0000

    👴🛬 Checkpoint ./server.js:2521121/1716 ./package.json:2521121/7006

--- server.js
+++ server.js
@@ -5,19 +5,55 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MLAB_URI)
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const exerciseSchema = mongoose.Schema({
+  userId: {type: String},
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: Date, required: false}
+})
+
+const userSchema = mongoose.Schema({
+  userId: {type: String, required: true},
+  exercises: {type: []}
+})
+
+const USER = mongoose.model('USER', userSchema);
+const EXERCISE = mongoose.model('EXERCISE', exerciseSchema);
+
+app.route('/api/exercise/new-user/')
+  .post(function(req, res){
+  let u = new USER;
+  u.userId = req.body.username;
+  console.log(u)
+  u.save(function (err,user){
+    if (err){return res.json({"error": err})}
+    return res.json(user)
+  })
+})
+
+app.post('/api/exercise/add/',function(req,res){
+  USER.find({userId: req.body.userId}, function(err, user){
+    if(err){return res.json({"error": err})}
+    user.exercises.push(new EXERCISE({userId: req.body.userId, description: req.body.description, duration: req.body.duration, date: new Date(req.body.date)}))
+    res.send("Exercise added for " + user.userId);
+  })
+})
+  
+app.get('/api/exercise/log/',function(req,res){
+  EXERCISE.find({userId: req.query.userId})
+})
 
 // Not found middleware
 app.use((req, res, next) => {
